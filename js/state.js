c3491853b33f7b82cc2ba8851ba6bd647dc806b1
PhilipStefanov <256216772+PhilipStefanov@users.noreply.github.com>
2026-05-09 23:59:24 +0200

    Implemented Player and Score functions

--- js/state.js
+++ js/state.js
@@ -1,13 +1,63 @@
 //stores all scores, no UI, no DOM, no logic
-export const CATEGORIES = [
-    "ones",
-    "twos"
+const CATEGORIES = [
+    {id: 'ones', label: 'Einser', type: 'input'},
+    {id: 'twos', label: 'Zweier', type: 'input'},
 ];
 
-export const state = {
-    players: [],
-    currentPlayerIndex: 0,
+const State = (() => {
 
-    //scores[playerIndex][category]
-    scores: []
-};
+    let players = [];
+    let nextID = 1;
+
+    /*
+    player = {
+        id: 1,
+        name: Philip,
+        scores = {ones: 3, fullHouse: 25}
+    }
+    */
+
+    function addPlayer(name){
+        players.push({
+            id: nextID, 
+            name: name, 
+            scores: {} 
+        });
+        nextID++;
+    }
+
+    function setScore(playerID, categoryID, value){
+        const player = players.find(p => p.id === playerID); //find player with matching id
+        if (!player) return; //if no player exists, stop
+        player.scores[categoryID] = value;
+    }
+
+    function getScore(playerID, categoryID){
+        const player = players.find(p => p.id === playerID);
+        if (!player) return;
+        return player.scores[categoryID];
+    }
+
+    function removeScore(playerID, categoryID){
+        const player = players.find(p => p.id === playerID);
+        if (!player) return;
+        delete player.scores[categoryID];
+    }
+
+    function renamePlayer(playerID, newName){
+        const player = players.find(p => p.id === playerID);
+        if (!player) return;
+        player.name = newName;
+    }
+
+    function removePlayer(playerID){
+        players = players.filter(p => p.id !== playerID);
+    }
+
+    function getPlayers(){
+        return players;
+    }
+
+    return { addPlayer, setScore, removeScore, getPlayers, getScore, renamePlayer, removePlayer };
+})();
+
