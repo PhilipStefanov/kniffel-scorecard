9c1771c23cf15e81bcf682480ae3bb5519ead845
PhilipStefanov <256216772+PhilipStefanov@users.noreply.github.com>
2026-05-09 23:58:15 +0200

    Implemented sum function

--- js/scoring.js
+++ js/scoring.js
@@ -1,21 +1,13 @@
 //only calculation logic
-import { CATEGORIES, state } from "./state.js";
 
-export function addPlayer(name){
-    state.players.push(name);
-    state.scores.push(
-        CATEGORIES.map(() => null)
-    );
+const IDs = ['ones', 'twos'];
+
+export function sum(ids, scores){
+    let s = 0;
+
+    ids.forEach(id => {
+        s += scores[id] ?? 0;
+    });
+
+    return s;
 }
-
-export function nextPlayer(){
-    state.currentPlayerIndex = 
-        (state.currentPlayerIndex + 1) % state.players.length
-}
-
-export function setScore(category, value){
-    const player = state.currentPlayerIndex;
-    const categoryIndex = CATEGORIES.indexOf(category)
-    state.scores[player][categoryIndex] = value;
-}
-
