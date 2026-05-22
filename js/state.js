d9d48cc135e9c5c11ab0dffe5e90255b3945deed
PhilipStefanov <256216772+PhilipStefanov@users.noreply.github.com>
2026-05-22 18:19:11 +0200

    Added lower section categories

--- js/state.js
+++ js/state.js
@@ -1,14 +1,23 @@
 //stores all scores, no UI, no DOM, no logic
 const CATEGORIES = [
-    {id: 'ones', label: 'Einser', type: 'input', val: '1'},
-    {id: 'twos', label: 'Zweier', type: 'input', val: '2'},
-    {id: 'threes', label: 'Dreier', type: 'input', val: '3'},
-    {id: 'fours', label: 'Vierer', type: 'input', val: '4'},
-    {id: 'fives', label: 'Fünfer', type: 'input', val: '5'},
-    {id: 'sixes', label: 'Sechser', type: 'input', val: '6'},
-    {id: 'gesamt', label: 'Gesamt', type: 'calc'},
+    {id: 'ones', label: 'Einser', type: 'input', val: '1', section: 'upper'},
+    {id: 'twos', label: 'Zweier', type: 'input', val: '2', section: 'upper'},
+    {id: 'threes', label: 'Dreier', type: 'input', val: '3', section: 'upper'},
+    {id: 'fours', label: 'Vierer', type: 'input', val: '4', section: 'upper'},
+    {id: 'fives', label: 'Fünfer', type: 'input', val: '5', section: 'upper'},
+    {id: 'sixes', label: 'Sechser', type: 'input', val: '6', section: 'upper'},
+    {id: 'gesamt', label: 'Gesamt oberer Teil', type: 'calc'},
     {id: 'bonus', label: 'Bonus', type: 'calc'},
-    {id: 'upper-total', label: 'Gesamt Up', type: 'calc'}
+    {id: 'upper-total', label: 'Gesamt Up', type: 'calc'},
+    {id: 'three-oak', label: 'Dreierpasch', type: 'input', section: 'lower'},
+    {id: 'four-oak', label: 'Viererpasch', type: 'input', section: 'lower'},
+    {id: 'fullhouse', label: 'Full House', type: 'input', section: 'lower'},
+    {id: 'smallStreet', label: 'Kleine Straße', type: 'input', section: 'lower'},
+    {id: 'bigStreet', label: 'Große Straße', type: 'input', section: 'lower'},
+    {id: 'kniffel', label: 'Kniffel', type: 'input', section: 'lower'},
+    {id: 'chance', label: 'Chance', type: 'input', section: 'lower'},
+    {id: 'gesamtUnten', label: 'Gesamt unterer Teil', type: 'calc'},
+    {id: 'total', label: 'Gesamte Punktezahl', type: 'calc'}
 ];
 
 const State = (() => {
@@ -32,6 +41,8 @@ const State = (() => {
         });
         nextID++;
     }
+
+
 
     function setScore(playerID, categoryID, value){
         const player = players.find(p => p.id === playerID); //find player with matching id
